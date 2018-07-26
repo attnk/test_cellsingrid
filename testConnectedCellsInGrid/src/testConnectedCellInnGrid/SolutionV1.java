f8296db627f0434bcf728ffe161c1ba1363b0b5a
Unknown <adritanaka@gmail.com>
2018-07-26 01:13:37 -0300

    Ajustando formato para utilizar um objeto auxiliar indicando o grupo

--- testConnectedCellsInGrid/src/testConnectedCellInnGrid/SolutionV1.java
+++ testConnectedCellsInGrid/src/testConnectedCellInnGrid/SolutionV1.java
@@ -1,52 +1,22 @@
 package testConnectedCellInnGrid;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Scanner;
-import java.util.stream.IntStream;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
 public class SolutionV1 {
 
-    // Complete the connectedCell function below.
-    static int connectedCell(int[][] matrix) {
-    	int max = 0, 
-			aux = 0;
-    	
-    	int[][] countAux = new int[matrix.length][matrix[0].length];
-    	int matrixMaxSlots = (matrix.length*matrix[0].length);
-    	
-//    	if(matrixMaxSlots == Arrays.stream(
-//    			matrixToArray(matrixMaxSlots, matrix))
-//    			.filter(x -> x == 1).count()) {
-//    		
-//    		max = matrixMaxSlots;
-//    	} else {
-    		for (int i = 0; i < matrix.length; i++) {
-    			for (int j = 0; j < matrix[i].length; j++) {
-    				if(matrix[i][j] ==  1) {
-    					aux = getLastTotal(matrix, aux, countAux, i, j);
-    					aux++;
-    				} else {
-    					aux = 0;
-    				}
-    				countAux[i][j] = aux;
-    			}
-    		}
-    		max = Arrays.stream(matrixToArray(matrixMaxSlots, countAux)).max().getAsInt();
-//    	}
-    	
-    	return max;
-    }
-
-    /**
+	/**
      * 
      * @param matrix
      * @param count
      * @param countAux
      * @param row
      * @param column
+     * @param cells
      * @return
      */
 	private static int getLastTotal(
@@ -54,70 +24,122 @@ public class SolutionV1 {
 			int count, 
 			int[][] countAux, 
 			int row, 
-			int column) {
+			int column,
+			List<Cell> cells) {
 		
 		int maxColumn = matrix[row].length;
+		Cell cell = new Cell(row, column);
 		
 		if(row == 0) {
 			if(column == 0 && matrix[row][column] == 1) {
+				cell.setGroup(1);
 				count = countAux[row][column];
 				
 			} else if(column > 0 && matrix[row][column] == 1) {
 				if(matrix[row][column-1] == 1) {
+					cell.setGroup(getGroupTo(row, column-1, cells));
 					count = countAux[row][column-1];
 					
+				} else {
+					cell.setGroup(getLastGridNumber(cells, cell));
 				}
 			}
 		} else if(row > 0) {
 			if(column == 0 && matrix[row][column] == 1) { 
 				if(matrix[row-1][column] == 1) {
-					count = countAux[row-1][maxColumn-1] <= 1 ?
-							countAux[row-1][column] : countAux[row-1][maxColumn-1];
-							
+					if(countAux[row-1][maxColumn-1] <= 1) {
+						cell.setGroup(getGroupTo(row-1, column, cells));
+						count = countAux[row-1][column];
+						
+					} else {
+						cell.setGroup(getGroupTo(row-1, maxColumn-1, cells));
+						count = countAux[row-1][maxColumn-1];
+					}
 				} else if(column < maxColumn-1 && matrix[row-1][column+1] == 1) {
-					count = countAux[row-1][maxColumn-1] <= 1 ? 
-							countAux[row-1][column+1] : countAux[row-1][maxColumn-1];
+					if(countAux[row-1][maxColumn-1] <= 1) {
+						cell.setGroup(getGroupTo(row-1, column+1, cells));
+						count = countAux[row-1][column+1];
+						
+					} else {
+						cell.setGroup(getGroupTo(row-1, maxColumn-1, cells));
+						count = countAux[row-1][maxColumn-1];
+					}
 				}
 			} else if(column > 0 && matrix[row][column] == 1) {
+				List<Cell> listAux = new ArrayList<>();
+				Cell cellAux = null;
 				
 				if(matrix[row][column-1] == 1) {
+					cell.setGroup(getGroupTo(row, column-1, cells));
 					count = countAux[row][column-1];
 					
 				} else if(column < maxColumn-1 && matrix[row-1][column+1] == 1) {
-					count = countAux[row-1][column+1];
+					listAux = cells.stream().filter(c -> c.getGroup() == 1).collect(Collectors.toList());
+					cellAux = listAux.get(listAux.size()-1);
 					
-				} else {
-					// verificar se já existe alguém contabilizado
-					int aux = 0, 
-						position = column-1;
-					
-					while(position >= 0) {
-						if(matrix[row][position] == 0) {
-							aux++;
-						}
-						position--;
+					if(cellAux.getGroup() == getGroupTo(row-1, column, cells)) {
+						cell.setGroup(getGroupTo(cellAux.getRow(), cellAux.getColumn(), cells));
+						count = countAux[cellAux.getRow()][cellAux.getColumn()];
+					} else {
+						cell.setGroup(getGroupTo(row-1, column, cells));
+						count = countAux[row-1][column+1];
 					}
 					
-					for(int i = (column - aux); i < aux; i++) {
-						if(matrix[row-1][i] == 1 && count < countAux[row][i > 0 ? i-1 : i]) {
-								count = countAux[row][i > 0 ? i-1 : i];
-						}
+				} else if(matrix[row-1][column] == 1) {
+					listAux = cells.stream().filter(c -> c.getGroup() == 1).collect(Collectors.toList());
+					cellAux = listAux.get(listAux.size()-1);
+					
+					if(cellAux.getGroup() == getGroupTo(row-1, column, cells)) {
+						cell.setGroup(getGroupTo(cellAux.getRow(), cellAux.getColumn(), cells));
+						count = countAux[cellAux.getRow()][cellAux.getColumn()];
+					} else {
+						cell.setGroup(getGroupTo(row-1, column, cells));
+						count = countAux[row-1][column];
 					}
 					
-					if(count == 0) {
-						if(matrix[row-1][column] == 1) {
-							count = countAux[row-1][column];
-							
-						} else if(matrix[row-1][column-1] == 1) {
-							count = countAux[row-1][column-1];
-							
-						} 
+				} else if(matrix[row-1][column-1] == 1) {
+					listAux = cells.stream().filter(c -> c.getGroup() == 1).collect(Collectors.toList());
+					cellAux = listAux.get(listAux.size()-1);
+					
+					if(cellAux.getGroup() == getGroupTo(row-1, column, cells)) {
+						cell.setGroup(getGroupTo(cellAux.getRow(), cellAux.getColumn(), cells));
+						count = countAux[cellAux.getRow()][cellAux.getColumn()];
+					} else {
+						cell.setGroup(getGroupTo(row-1, column, cells));
+						count = countAux[row-1][column-1];
 					}
-				}
+				} 
 			}
 		}
+		cells.add(cell);
+		
 		return count;
 	}
+
+
+	/**
+	 * 
+	 * @param row
+	 * @param column
+	 * @param cells
+	 * @return
+	 */
+	private static int getGroupTo(int row, int column, List<Cell> cells) {
+		int result;
+		
+		try {
+			result = cells.stream()
+					.filter(c -> c.getRow() == row && c.getColumn() == column)
+					.findFirst()
+					.get().getGroup();
+			
+		} catch (NoSuchElementException e) {
+			result = cells.stream().mapToInt(Cell::getGroup).max().getAsInt()+1;
+		}
+		
+		return result;
+	}
+
 
 	/**
 	 * 
@@ -140,9 +162,81 @@ public class SolutionV1 {
 			System.out.println();
 		}
         
-        
 		return auxArray;
 	}
+
+	/**
+	 * 
+	 * @param cells
+	 * @param cell
+	 */
+	private static int getLastGridNumber(List<Cell> cells, Cell cell) {
+		int group = 0;
+		
+		if(cells.isEmpty()) {
+			group = 1;
+		} else {
+			group = cells.get(cells.size()-1).getGroup();
+		}
+		
+		return group;
+	}
+	
+	private static class Cell{
+		
+		private int column;
+		private int row;
+		private int group;
+		
+		public Cell(int row, int column) {
+			this.row = row;
+			this.column = column;
+			this.group = 0;
+		}
+
+		public int getColumn() {
+			return column;
+		}
+		
+		public int getRow() {
+			return row;
+		}
+		
+		public int getGroup() {
+			return group;
+		}
+		
+		public void setGroup(int group) {
+			this.group = group;
+		}
+	}
+	
+    // Complete the connectedCell function below.
+    static int connectedCell(int[][] matrix) {
+    	int max = 0, 
+			aux = 0;
+    	
+    	List<Cell> cellls = new ArrayList<>();
+    	
+    	int[][] countAux = new int[matrix.length][matrix[0].length];
+    	int matrixMaxSlots = (matrix.length*matrix[0].length);
+    	
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[i].length; j++) {
+				if(matrix[i][j] ==  1) {
+					aux = getLastTotal(matrix, aux, countAux, i, j, cellls);
+					aux++;
+				} else {
+					aux = 0;
+				}
+				countAux[i][j] = aux;
+			}
+			aux = 0;
+		}
+		max = Arrays.stream(matrixToArray(matrixMaxSlots, countAux)).max().getAsInt();
+    	
+    	return max;
+    }
 
 //    private static final Scanner scanner = new Scanner(System.in);
 
@@ -160,6 +254,38 @@ public class SolutionV1 {
     	
         int[][] matrix = new int[n][m];
 
+//        matrix[0][0] = 1;
+//        matrix[0][1] = 1;
+//        matrix[0][2] = 0;
+//        matrix[0][3] = 0;
+//        matrix[0][4] = 0;
+//        
+//        matrix[1][0] = 0;
+//        matrix[1][1] = 1;
+//        matrix[1][2] = 1;
+//        matrix[1][3] = 0;
+//        matrix[1][4] = 0;
+//        
+//        matrix[2][0] = 0;
+//        matrix[2][1] = 0;
+//        matrix[2][2] = 1;
+//        matrix[2][3] = 0;
+//        matrix[2][4] = 1;
+//        
+//        matrix[3][0] = 1;
+//        matrix[3][1] = 0;
+//        matrix[3][2] = 0;
+//        matrix[3][3] = 0;
+//        matrix[3][4] = 1;
+//        
+//        matrix[4][0] = 0;
+//        matrix[4][1] = 1;
+//        matrix[4][2] = 0;
+//        matrix[4][3] = 1;
+//        matrix[4][4] = 1;
+        
+        
+        // -------------- 15 
         matrix[0][0] = 0;
         matrix[0][1] = 1;
         matrix[0][2] = 1;
@@ -189,6 +315,7 @@ public class SolutionV1 {
         matrix[4][2] = 1;
         matrix[4][3] = 1;
         matrix[4][4] = 0;
+        
         
 //        for (int i = 0; i < n; i++) {
 //            String[] matrixRowItems = scanner.nextLine().split(" ");
